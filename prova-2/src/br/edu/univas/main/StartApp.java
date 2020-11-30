af1eafd2d2e8acb1799aa6fec7251dc23cf97115
Eduardo-FCS <eduardofariacaetano100@gmail.com>
2020-11-30 20:37:35 -0300

    Upando scanner

--- prova-2/src/br/edu/univas/main/StartApp.java
+++ prova-2/src/br/edu/univas/main/StartApp.java
@@ -1,10 +1,14 @@
 package br.edu.univas.main;
 
+import java.util.Scanner;
+
 public class StartApp {
 	
 	public static void main(String[] args) {
 		
-		
+		Scanner scanner = new Scanner(System.in);
+
+		scanner.close();
 	}
 
 }
